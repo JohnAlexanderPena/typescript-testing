ceb58e6d3ca8ab53a0009879708876b6585e1a08
jpena91@gmail.com <jpena91@gmail.com>
2021-05-01 12:13:00 -0400

    test funcions

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactNode, ReactElement } from 'react';
 import './App.css';
+
+//Conventional Props
+
+
+function Heading({ title }: {title?: string;}){
+  return <h1>{title}</h1>
+}
+
+
+function HeadingWithContent({ children }: {children: ReactNode}): ReactElement {
+  return <h1>{children}</h1>
+}
+
+
+//Default Props
+
+const defaultContainerProps = {
+  heading: <strong>My Heading</strong>
+}
+function Container({ heading, children }: {children: ReactNode} & typeof defaultContainerProps): ReactElement {
+  return <div><h1>{heading}{children}</h1>{children}</div>
+}
+
+Container.defaultProps = defaultContainerProps;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     <Heading title={"Hello There"}></Heading>
+     <HeadingWithContent><strong>Check This!</strong></HeadingWithContent>
+     <Container>Foo</Container>
     </div>
   );
 }
